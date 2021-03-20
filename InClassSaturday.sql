e7636ab4ec1415d031f0dc64679e63cc38adf1f2
Chris LoJacono <clojacono92@gmail.com>
2021-03-20 09:49:31 -0500

    another save

--- InClassSaturday.sql
+++ InClassSaturday.sql
@@ -21,3 +21,21 @@ from Author A
 	join Grade G
 	on G.Id = A.GradeId
 order by A.Name asc
+
+--6. Starting with the above query, add the recorded gender of each of the authors.
+select top 76 A.Name, G.Name, Ge.Name
+from Author A
+	join Grade G
+	on G.Id = A.GradeId
+	join Gender Ge
+	on A.GenderId = Ge.Id
+order by A.Name asc
+
+--7.What is the total number of words in all poems in the database?
+select sum(p.WordCount)
+from Poem P
+
+--8. Which poem has the fewest characters?
+select P.Title, P.CharCount
+from Poem P
+order by P.CharCount asc
